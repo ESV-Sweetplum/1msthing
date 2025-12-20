9aec6a393f5433f305643fdaf8bb0df7fe5a1dcb
plummyyummy <sweetplumquaver@gmail.com>
2025-12-20 15:29:16 -0800

    fix mines not moving correctly

--- plugin.lua
+++ plugin.lua
@@ -8,32 +8,32 @@ function draw()
         local result = {}
 
         for _, note in pairs(notes) do
-            if note.EndTime == 0 then
-                table.insert(result, utils.CreateHitObject(note.StartTime + 1, note.Lane, note.EndTime, note.HitSound, note.EditorLayer))
-            else
-                table.insert(result, utils.CreateHitObject(note.StartTime + 1, note.Lane, note.EndTime + 1, note.HitSound, note.EditorLayer))
-            end
+            table.insert(result,
+                utils.CreateHitObject(note.StartTime + 1, note.Lane, note.EndTime == 0 and 0 or note.EndTime + 1,
+                    note.HitSound, note.EditorLayer, note.Type))
         end
 
-        actions.RemoveHitObjectBatch(notes)
-        actions.PlaceHitObjectBatch(result)
+        actions.PerformBatch({
+            utils.CreateEditorAction(action_type.RemoveHitObjectBatch, notes),
+            utils.CreateEditorAction(action_type.PlaceHitObjectBatch, result),
+        })
         actions.SetHitObjectSelection(result)
     end
-    
+
     if imgui.Button("down") then
         local notes = state.SelectedHitObjects
         local result = {}
 
         for _, note in pairs(notes) do
-            if note.EndTime == 0 then
-                table.insert(result, utils.CreateHitObject(note.StartTime - 1, note.Lane, note.EndTime, note.HitSound, note.EditorLayer))
-            else
-                table.insert(result, utils.CreateHitObject(note.StartTime - 1, note.Lane, note.EndTime - 1, note.HitSound, note.EditorLayer))
-            end
+            table.insert(result,
+                utils.CreateHitObject(note.StartTime - 1, note.Lane, note.EndTime == 0 and 0 or note.EndTime - 1,
+                    note.HitSound, note.EditorLayer, note.Type))
         end
 
-        actions.RemoveHitObjectBatch(notes)
-        actions.PlaceHitObjectBatch(result)
+        actions.PerformBatch({
+            utils.CreateEditorAction(action_type.RemoveHitObjectBatch, notes),
+            utils.CreateEditorAction(action_type.PlaceHitObjectBatch, result),
+        })
         actions.SetHitObjectSelection(result)
     end
 
